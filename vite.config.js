c3d47e3c96b3821fd29391b91563d4a224d9c633
Terrence Hannahan <thannahan@gmail.com>
2023-07-03 00:30:37 -0400

    finally done...things are looking good!

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]', 
+      },
+    },
+  },
+  resolve: {
+    alias: {
+            'pix': '/src/images',
+    },
+  },
+  optimizeDeps: {
+    include: ['pix/scoob.gif'], 
+  },
 })
